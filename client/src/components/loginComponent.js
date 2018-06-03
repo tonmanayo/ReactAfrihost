30384499a26c160fdf5e0b54d2e46027d328107b
tonmanayo <tonmanayo@hotmail.com>
2018-06-03 13:54:57 +0200

    login working with react

--- client/src/components/loginComponent.js
+++ client/src/components/loginComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap'
-import { FormControl, FormGroup, ControlLabel, HelpBlock } from 'react-bootstrap'
+import {FormControl, FormGroup, ControlLabel, HelpBlock } from 'react-bootstrap'
 
 function FieldGroup({ id, label, help, ...props }) {
     return (
@@ -12,30 +12,82 @@ function FieldGroup({ id, label, help, ...props }) {
     );
 }
 
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return response
+    } else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        throw error
+    }
+}
+
+function parseJSON(response) {
+    return response.json()
+}
+
+function loginVerify(username, password) {
+    fetch('http://localhost:3001/auth/signin', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })})
+        .then(checkStatus)
+        .then(parseJSON)
+        .then(function(data) {
+            console.log('request succeeded with JSON response', data)
+        }).catch(function(error) {
+        console.log('request failed', error)
+    })
+}
+
 class LoginComponent extends Component {
     constructor(props, context) {
         super(props, context);
 
-        this.handleUsername = this.handleUsername.bind(this);
-        this.handlePassword = this.handlePassword.bind(this);
-
-        this.state = {
+       this.handleUsername = this.handleUsername.bind(this);
+       this.handlePassword = this.handlePassword.bind(this);
+       this.loginVerify = this.loginVerify.bind(this);
+       this.state = {
             username: '',
             password:''
-        };
-    }
-
-    getValidationState() {
-        const length = this.state.password.length;
-        if (length > 10) return 'success';
-        else if (length > 5) return 'warning';
-        else if (length > 0) return 'error';
-        return null;
+       };
     }
 
     loginVerify() {
-        //if (this.state.password)
+        console.log(this.state);
+        fetch('http://localhost:3001/auth/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password
+            })})
+            .then(checkStatus)
+            .then(parseJSON)
+            .then(function(data) {
+                console.log('request succeeded with JSON response', data)
+            }).catch(function(error) {
+            console.log('request failed', error)
+        })
     }
+
+
+    // getValidationState() {
+    //     const length = this.state.password.length;
+    //     if (length > 10) return 'success';
+    //     else if (length > 5) return 'warning';
+    //     else if (length > 0) return 'error';
+    //     return null;
+    // }
+
+
 
     handleUsername(e) {
         this.setState({ username: e.target.value });
@@ -48,10 +100,10 @@ class LoginComponent extends Component {
     render() {
         return (
             <div className="App">
-                <form>
+
                     <FormGroup
                         controlId="formBasicText"
-                        validationState={this.getValidationState()}
+                        //validationState={this.getValidationState()}
                     >
                         <ControlLabel>Enter User Name and Password</ControlLabel>
                         <form>
@@ -60,23 +112,27 @@ class LoginComponent extends Component {
                                 type="text"
                                 label="username"
                                 placeholder="Enter Username"
-                                value={this.state.value}
+                                value={this.state.username}
                                 onChange={this.handleUsername}
                             />
                             <FieldGroup
                                 id="formControlsText"
                                 type="password"
                                 label="Password"
+                                value={this.state.password}
                                 placeholder="Enter Password"
                                 onChange={this.handlePassword}
                             />
 
-                            <Button bsStyle="primary" type="submit" >Submit</Button>
-                        </form>
+                            <Button onClick={
+                                this.loginVerify
+                            } bsStyle="primary"  >Submit</Button>
+
+                                </form>
                         <FormControl.Feedback />
                         <HelpBlock>Validation is based on string length.</HelpBlock>
                     </FormGroup>
-                </form>
+
             </div>
         );
     }
--- server/index.js
+++ server/index.js
@@ -9,12 +9,19 @@ import authRoutes from'./routes/auth';
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/afrihost');
-
 app.use(logger('dev'));
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
 
 app.use('/auth', authRoutes);
 
-app.listen(6000, () => console.log('Example app listening on port 6000!'));
+app.listen(3001, () => console.log('Example app listening on port 3001!'));
